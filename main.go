345b962857169a0b9e1db1f3d3fbe66f2c7a6a98
vickiller-droid <victordanielgamco.com>
2026-04-09 03:47:08 +0100

    Printed different value type including strings which could be added with +

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("1+1 = 2", 1+1 == 2)
+	fmt.Println("30345/29393", 30345/29393)
+	fmt.Println("true && true")
+	fmt.Println("true || false")
+	fmt.Println(!true)
+	fmt.Println(true)
+}
